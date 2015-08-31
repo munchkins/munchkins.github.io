5c93f6484b18f7c0993df281f217373fbb4a8cef
Jaco Greeff <jacogr@gmail.com>
2015-08-31 13:13:56 +0200

    limit resources to cap

--- src/js/controllers/game.js
+++ src/js/controllers/game.js
@@ -4,13 +4,16 @@ angular
     const tickloop = function() {
       Game.ticks++;
 
-      angular.forEach(Resources, function(key, res) {
-        console.log(key, res.value);
+      angular.forEach(Resources, function(res) {
         res.value.current += res.rate;
+        if (res.value.limit) {
+          res.value.current = Math.min(res.value.current, res.value.limit);
+        }
       });
     };
 
     Storage.load();
+
     $interval(Storage.save, Defaults.SAVE_RATE);
     $interval(tickloop, Defaults.TICK_RATE);
   });
