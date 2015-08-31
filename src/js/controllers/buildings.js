80f79f095f7e5ca791ab0ca9b89a616ff6c860a2
Jaco Greeff <jacogr@gmail.com>
2015-08-31 12:28:07 +0200

    simple resource view

--- src/js/controllers/buildings.js
+++ src/js/controllers/buildings.js
@@ -1,5 +1,21 @@
 angular
   .module('munchkins.controllers')
-  .controller('Buildings', function(Buildings) {
+  .controller('Buildings', function(Buildings, Resources) {
     this.buildings = Buildings;
+
+    this.buy = function(key) {
+      const b = Buildings[key];
+      b.value.current++;
+
+      _.forEach(b.requires.resources, function(req, rkey) {
+        const r = Resources[rkey];
+        r.value.current -= req.value;
+      });
+
+      _.forEach(b.provides.resources, function(prov, pkey) {
+        const r = Resources[pkey];
+        r.value.current++;
+        r.rate += prov.rate;
+      });
+    };
   });
---
+++ src/js/controllers/resources.js
@@ -0,0 +1,5 @@
+angular
+  .module('munchkins.resources')
+  .controller('Resources', function(Resources) {
+    this.resources = Resources;
+  });
