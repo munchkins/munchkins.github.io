4dbbb7a9c3dc46c13eaebbf18cd43b554ce9d4a7
Jaco Greeff <jacogr@gmail.com>
2015-08-31 17:04:50 +0200

    numeric filter in correct places

--- src/js/filters/numeric.js
+++ src/js/filters/numeric.js
@@ -5,8 +5,8 @@ angular
 
     return function(number, precision) {
       const n = number || 0;
-      const u = Math.floor(Math.log(n) / Math.log(1000));
-      
+      const u = Math.floor(Math.log(n * 1000) / Math.log(1000)) - 1;
+
       let p = precision || ((precision === 0) ? 0 : 2);
       if ((p === 0) && (p >= 1000)) {
         p = 2;
