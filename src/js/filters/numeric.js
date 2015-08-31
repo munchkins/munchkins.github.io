8746dbfc8848cb38b92b9f3dccf5a76f478723bf
Jaco Greeff <jacogr@gmail.com>
2015-08-31 17:12:13 +0200

    simplify small number formatting

--- src/js/filters/numeric.js
+++ src/js/filters/numeric.js
@@ -5,13 +5,13 @@ angular
 
     return function(number, precision) {
       const n = number || 0;
-      const u = Math.floor(Math.log(n * 1000) / Math.log(1000)) - 1;
+      const u = Math.floor(Math.log(n) / Math.log(1000));
 
       let p = precision || ((precision === 0) ? 0 : 2);
       if ((p === 0) && (p >= 1000)) {
         p = 2;
       }
 
-      return (((n * 1000) / 1000 ** Math.floor(u)) / 1000).toFixed(p) + units[u];
+      return (u < 0) ? (n.toFixed(p)) : ((n / 1000 ** Math.floor(u)).toFixed(p) + units[u]);
     };
   });
