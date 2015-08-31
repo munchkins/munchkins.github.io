4c66c3271d4a8faedd17afd0bf65e3c6aff3708d
Jaco Greeff <jacogr@gmail.com>
2015-08-31 16:57:17 +0200

    try to fix building number display

--- src/js/filters/numeric.js
+++ src/js/filters/numeric.js
@@ -4,8 +4,12 @@ angular
     const units = ['', 'K', 'M', 'G', 'T', 'P'];
 
     return function(number, precision) {
-      const p = precision || 2;
       const u = Math.floor(Math.log(number) / Math.log(1000));
+      let p = precision || 2;
+
+      if ((p === 0) && (p >= 1000)) {
+        p = 2;
+      }
 
       return (number / 1000 ** Math.floor(u)).toFixed(p) + '' + units[u];
     };
