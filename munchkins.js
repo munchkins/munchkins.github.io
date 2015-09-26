fba56643be385f15593d221e11cd075b63b2f44b
Jaco Greeff <jacogr@gmail.com>
2015-09-26 10:08:25 +0200

    space menu, game wipe functionality

--- munchkins.js
+++ munchkins.js
@@ -64,6 +64,7 @@ angular.module('munchkins').controller('Subbar', ["$location", "Buildings", "Tri
 
 angular.module('munchkins').controller('Topbar', ["Game", function (Game) {
   this.save = Game.save;
+  this.wipe = Game.wipe;
   this.calendar = Game.calendar;
   this.bonus = Game.bonus;
 }]);
--- src/js/controllers/topbar.js
+++ src/js/controllers/topbar.js
@@ -2,6 +2,7 @@ angular
   .module('munchkins')
   .controller('Topbar', function(Game) {
     this.save = Game.save;
+    this.wipe = Game.wipe;
     this.calendar = Game.calendar;
     this.bonus = Game.bonus;
   });
