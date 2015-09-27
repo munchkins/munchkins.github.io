c870b2c18012a907bf223469109d65af9a41a6d3
Jaco Greeff <jacogr@gmail.com>
2015-09-27 09:25:34 +0200

    buildings & tribe tested

---
+++ spec/js/services/buildings.spec.js
@@ -0,0 +1,70 @@
+describe('Buildings', () => {
+  let Buildings;
+
+  beforeEach(() => {
+    module('munchkins');
+
+    inject(($injector) => {
+      Buildings = $injector.get('Buildings');
+    });
+  });
+
+  describe('.all', () => {
+    it('returns all resources', () => {
+      const all = Buildings.all();
+      expect(all).to.be.ok;
+      expect(Object.keys(all).length).to.be.at.least(1);
+    });
+  });
+
+  describe('.get', () => {
+    it('returns a specific resource', () => {
+      const item = Buildings.get('shelter');
+      expect(item).to.be.ok;
+    });
+  });
+
+  describe('.save', () => {
+    it('saves values', () => {
+      const item = Buildings.get('shelter');
+      item.locked = 'locked';
+      item.value = 1234;
+
+      const save = {};
+      Buildings.save(save);
+
+      expect(save.shelter.locked).to.equal('locked');
+      expect(save.shelter.value).to.equal(1234);
+    });
+  });
+
+  describe('.load', () => {
+    it('loads values', () => {
+      const item = Buildings.get('shelter');
+
+      const load = { shelter: { locked: 'locked', value: 1234 }};
+      Buildings.load(load);
+
+      expect(item.locked).to.equal('locked');
+      expect(item.value).to.equal(1234);
+    });
+
+    it('ignores unknowns', () => {
+      const load = { unknown: { locked: 'locked', value: 1234 }};
+      Buildings.load(load);
+    });
+  });
+
+  describe('.activeTotal', () => {
+    it('has no unlocks at the start', () => {
+      expect(Buildings.activeTotal()).to.equal(0);
+    });
+
+    it('returns the collect unlocks', () => {
+      Buildings.get('shelter').locked = false;
+      Buildings.get('fire').locked = false;
+
+      expect(Buildings.activeTotal()).to.equal(2);
+    });
+  });
+});
---
+++ spec/js/services/tribe.spec.js
@@ -0,0 +1,98 @@
+describe('Tribe', () => {
+  let Tribe;
+
+  beforeEach(() => {
+    module('munchkins');
+
+    inject(($injector) => {
+      Tribe = $injector.get('Tribe');
+    });
+  });
+
+  describe('.all', () => {
+    it('returns all resources', () => {
+      const all = Tribe.all();
+      expect(all).to.be.ok;
+      expect(Object.keys(all).length).to.be.at.least(1);
+    });
+  });
+
+  describe('.get', () => {
+    it('returns a specific resource', () => {
+      const item = Tribe.get('farmer');
+      expect(item).to.be.ok;
+    });
+  });
+
+  describe('.add, .free & .total', () => {
+    it('starts with 0 free', () => {
+      expect(Tribe.free()).to.equal(0);
+    });
+
+    it('starts with 0 total', () => {
+      expect(Tribe.total()).to.equal(0);
+    });
+
+    it('adds free on .add', () => {
+      Tribe.add(5);
+      expect(Tribe.free()).to.equal(5);
+    });
+
+    it('adds total on .add', () => {
+      Tribe.add(5);
+      expect(Tribe.total()).to.equal(5);
+    });
+
+    it('adds total on active', () => {
+      Tribe.get('farmer').value.current = 5;
+      expect(Tribe.total()).to.equal(5);
+    });
+  });
+
+  describe('.save', () => {
+    it('save free', () => {
+      Tribe.add(5);
+
+      const save = {};
+      Tribe.save(save);
+
+      expect(save.free).to.equal(5);
+    });
+
+    it('saves types', () => {
+      const item = Tribe.get('farmer');
+      item.locked = 'locked';
+      item.value = 1234;
+
+      const save = {};
+      Tribe.save(save);
+
+      expect(save.types.farmer.locked).to.equal('locked');
+      expect(save.types.farmer.value).to.equal(1234);
+    });
+  });
+
+  describe('.load', () => {
+    it('loads free', () => {
+      const load = { free: 5 };
+      Tribe.load(load);
+
+      expect(Tribe.free()).to.equal(5);
+    });
+
+    it('loads types', () => {
+      const item = Tribe.get('farmer');
+
+      const load = { types: { farmer: { locked: 'locked', value: 1234 }}};
+      Tribe.load(load);
+
+      expect(item.locked).to.equal('locked');
+      expect(item.value).to.equal(1234);
+    });
+
+    it('ignores unknowns', () => {
+      const load = { types: { unknown: { locked: 'locked', value: 1234 }}};
+      Tribe.load(load);
+    });
+  });
+});
