e71f0e8c4b7a527ce2c74351ac02e7030020b230
Jaco Greeff <jacogr@gmail.com>
2015-09-27 08:41:20 +0200

    () => {}

--- spec/js/filters/numeric.spec.js
+++ spec/js/filters/numeric.spec.js
@@ -1,61 +1,61 @@
-describe('Numeric Filter', function() {
+describe('Numeric Filter', () => {
   let filter = undefined;
 
-  beforeEach(function() {
+  beforeEach(() => {
     module('munchkins');
 
-    inject(function($filter) {
+    inject(($filter) => {
       filter = $filter('numeric');
     });
   });
 
-  it('exists', function() {
+  it('exists', () => {
     expect(filter).to.be.ok;
   });
 
-  it('returns 0.00 on no value', function() {
+  it('returns 0.00 on no value', () => {
     expect(filter()).to.eq('0.000');
   });
 
-  it('returns values with default precision', function() {
+  it('returns values with default precision', () => {
     expect(filter(123.4557)).to.eq('123.456');
   });
 
-  it('returns arbitrary precision', function() {
+  it('returns arbitrary precision', () => {
     expect(filter(55.6789, 0)).to.eq('56');
     expect(filter(55.6789, 1)).to.eq('55.7');
     expect(filter(55.6789, 5)).to.eq('55.67890');
   });
 
-  it('uses precision 3 when none specified for numbers > 1000', function() {
+  it('uses precision 3 when none specified for numbers > 1000', () => {
     expect(filter(12345, 0)).to.eq('12.345K');
   });
 
-  it('returns values < 1.0', function() {
+  it('returns values < 1.0', () => {
     expect(filter(0.59213)).to.eq('0.592');
   });
 
-  it('returns K values', function() {
+  it('returns K values', () => {
     expect(filter(1103)).to.eq('1.103K');
     expect(filter(9994)).to.eq('9.994K');
   });
 
-  it('returns M values', function() {
+  it('returns M values', () => {
     expect(filter(1103000)).to.eq('1.103M');
     expect(filter(9994000)).to.eq('9.994M');
   });
 
-  it('returns G values', function() {
+  it('returns G values', () => {
     expect(filter(1103000000)).to.eq('1.103G');
     expect(filter(9994000000)).to.eq('9.994G');
   });
 
-  it('returns T values', function() {
+  it('returns T values', () => {
     expect(filter(1103000000000)).to.eq('1.103T');
     expect(filter(9994000000000)).to.eq('9.994T');
   });
 
-  it('returns P values', function() {
+  it('returns P values', () => {
     expect(filter(1103000000000000)).to.eq('1.103P');
     expect(filter(9994000000000000)).to.eq('9.994P');
   });
