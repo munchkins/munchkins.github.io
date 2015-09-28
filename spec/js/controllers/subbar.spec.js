bb8452c5b602371366eff1dc6cf48d1b4fb4b8a5
Jaco Greeff <jacogr@gmail.com>
2015-09-28 09:19:20 +0200

    Subbar.iOn tested (first 100%)

--- spec/js/controllers/subbar.spec.js
+++ spec/js/controllers/subbar.spec.js
@@ -1,17 +1,30 @@
 describe('Subbar Controller', () => {
   let Ctrl;
+  let location;
 
   beforeEach(() => {
     module('munchkins');
 
-    inject(($controller) => {
+    inject(($controller, $location) => {
       Ctrl = $controller('Subbar');
+      location = $location;
     });
   });
 
   describe('init', () => {
     it('exists', () => {
       expect(Ctrl).to.be.ok;
+    });
+  });
+
+  describe('isOn', () => {
+    it('returns false when path does not match', () => {
+      expect(Ctrl.isOn('/test')).to.be.false;
+    });
+
+    it('returns true when path does match', () => {
+      location.path('/test');
+      expect(Ctrl.isOn('/test')).to.be.true;
     });
   });
 });
